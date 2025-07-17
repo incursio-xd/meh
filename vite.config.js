25a122a8e1b63a08b1efaf6c3d3dc480bf8cfea1
Aman <2330215@kiit.ac.in>
2025-07-17 19:30:01 +0530

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
+  base: '/meh/', // important for GitHub Pages deployment
   resolve: {
     alias: {
-      src: "/src",
-      lib: "/lib",
-      components: "/src/components",
-      assets: "/src/assets",
-      utils: "/utils",
+      src: path.resolve(__dirname, './src'),
+      lib: path.resolve(__dirname, './lib'),
+      components: path.resolve(__dirname, './src/components'),
+      assets: path.resolve(__dirname, './src/assets'),
+      utils: path.resolve(__dirname, './utils'),
     },
   },
 });
